ad39f6687ad1827e0cb06df970944704b9f9c234
monica-bawankule <54322100+monica-bawankule@users.noreply.github.com>
2019-08-26 14:15:56 +0530

    Add files via upload
    Adding of folders

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(function(){
+	$('.images').cycle({
+		fx:'fade',
+		timeout:1000,
+		speed:1000
+	});
+})
+
+$(function(){
+	$('.wrapper').cycle({
+		fx:'fade',
+		timeout:1000,
+		speed:1000
+	});
+})
